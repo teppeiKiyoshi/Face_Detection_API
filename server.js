6a84426dfbd6d925d42cb97d27c9b60b87af5aba
Eman Martin <martinemanuelleo@gmail.com>
2021-12-07 16:36:37 +0800

    changed response

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send(database.users);
+    res.send('its working');
 })
 
 app.post('/signin', (req, res) => {
