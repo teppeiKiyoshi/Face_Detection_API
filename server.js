6ff27f19650848cc52f765ef713b966681d20be6
Eman Martin <martinemanuelleo@gmail.com>
2021-12-07 16:34:17 +0800

    added changes for deployment

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.post('/imageurl', (req, res) => {
         .catch(err => res.status(400).json('image url not working'))
 })
 
-app.listen(3000, () => {
-    console.log('app is running on 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log('app is running on port ${process.env.PORT}');
 })
 
